267a89d074f001e44d5c6ce143ee8a4bc28f2ce2
Betuel <betuel.arones@tecsup.edu.pe>
2025-11-25 23:58:46 -0500

    Backend actualizado

--- target/generated-sources/annotations/com/tecsup/petclinic/mapper/PetMapperImpl.java
+++ target/generated-sources/annotations/com/tecsup/petclinic/mapper/PetMapperImpl.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-11-25T23:55:43-0500",
+    date = "2025-11-25T23:58:00-0500",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
 )
 @Component
